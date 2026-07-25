34760b140e5ad5ab9e33660bb4f9542f1997b07b
Nico Velasco <307379331+nvelascodev-cpu@users.noreply.github.com>
2026-07-25 15:17:14 -0500

    Add data cleaning transformation to ETL pipeline

--- src/etl/transform/data_transformer.py
+++ src/etl/transform/data_transformer.py
@@ -43,10 +43,26 @@ def normalize_text(df):
             )
     return df
 
-def tansform(df):
+def remove_empty_rows(df):
+    df=df.dropna(how="all")
+    return df
+
+def remove_null_ids(df):
+    df=df.dropna(subset=['id'])
+    return df
+
+def remove_duplicate_ids(df):
+    df=df.drop_duplicates(subset=['id'], keep='first')
+    return df
+
+def tansform(df): #basic transform
     df_transform = df.copy()
 
     df_transform = convert_data_types(df_transform,data_contract.EXPECTED_TYPES)
     df_transform = normalize_text(df_transform)
+    df_transform = remove_empty_rows(df_transform)
+    df_transform = remove_null_ids(df_transform)
+    df_transform = remove_duplicate_ids(df_transform)
+
 
     return df_transform
